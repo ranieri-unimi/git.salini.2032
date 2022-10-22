a79256038bc3d5490b9d10aac6ede109c02a46a8
Martin Ranieri <51285505+ranieri-unimi@users.noreply.github.com>
2022-10-22 17:57:55 +0200

    backup

--- csv2pivot.py
+++ csv2pivot.py
@@ -2,8 +2,8 @@
 import pandas as pd
 
 # %%
-df_team = pd.read_csv("datasets/matches.csv")
-all_pkmn = df_team.pokemon.drop_duplicates().to_list()
+df = pd.read_csv("datasets/matches.csv")
+all_pkmn = df.pokemon.drop_duplicates().to_list()
 
 # %%
 clust = pd.read_csv("datasets/clusters.csv")
@@ -12,20 +12,20 @@ all_clust = {e["pokemon"]: e["cluster"] for _, e in clust.iterrows()}
 
 # %%
 for e in all_clust:
-    df_team[e] = 0
+    df[e] = 0
 
 for e in set(all_clust.values()):
-    df_team[e] = 0
+    df[e] = 0
 
 # %%
-for i, row in df_team.iterrows():
+for i, row in df.iterrows():
     pkmn = row.pokemon
-    df_team.loc[i, pkmn] = 1
-    df_team.loc[i, all_clust[pkmn]] = 1
+    df.loc[i, pkmn] = 1
+    df.loc[i, all_clust[pkmn]] = 1
 
 # %%
-df_team = (
-    df_team.groupby(
+df = (
+    df.groupby(
         [
             "id_match",
             "win",
@@ -35,11 +35,11 @@ df_team = (
     .reset_index()
 )
 
-# %%
-for e in set(all_clust.values()):
-    df_team[e + "_pw2"] = df_team[e] ** 2
+# # %%
+# for e in set(all_clust.values()):
+#     df[e + "_pw2"] = df[e] ** 2
 
 # %%
-df_team.to_csv("datasets/pivot.group.matches.csv", index=False)
+df.to_csv("datasets/pivot.group.matches.csv", index=False)
 
 # %%
